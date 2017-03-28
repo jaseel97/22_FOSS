caa5cd15b17265d2d9eac59f111b60cf6d3e1aaa
Jaseel <s4csb@debian-40>
2017-03-28 15:13:24 +0530

    linear search

---
+++ perlWeek/perl6.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+# linear search
+print "Enter the elements. Press ctrl+d to stop input\n";
+@a = <STDIN>;
+print "Enter search key\n";
+$k = <STDIN>;
+chop $k;
+while ($a[$i] ne ""){
+	#print ".\n";
+	if ($f = $k == $a[$i]){
+		print "$k at position ".($i+1)."\n";
+		last;	
+	}
+	$i++;
+}
+unless ($f){
+	print "Search Failed\n";
+}
